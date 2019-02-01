127c150a2f43b34e7d7b5b2c32b993f7a08847e7
leekhanh1993 <lkgame1993@gmail.com>
2019-02-01 22:02:51 +0700

    commit

--- client/src/components/layouts/FooterPage.js
+++ client/src/components/layouts/FooterPage.js
@@ -1,26 +1,35 @@
 import React, { Component } from 'react';
+import logo from "../../images/picktune-logo.png"
 
 class FooterPage extends Component {
     render() {
         return (
             <div>
                 <footer className="mt-5 bg-dark p-5 text-light">
-                    <div className="footer-top">
-                        <div className="row">
-                            <div className="col-lg-3 col-md-6 footer-brand">
-                                <h3>Logo Website</h3>
-                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis, repellat corrupti non modi, sed exercitationem voluptates quia natus optio nemo voluptatem iste illo sit minus. Voluptates sapiente ex asperiores autem?</p>
-                            </div>
-                            <div className="col-lg-3 col-md-6 footer-links">
-                                <h4>Useful Links</h4>
-                                <ul>
-                                    <li>About us</li>
-                                    <li>Contact US</li>
-                                </ul>
+                    <div className="container">
+                        <div className="footer-top">
+                            <div className="row">
+                                <div className="col-lg-3 col-md-6 footer-brand">
+                                    <div className="text-center">
+                                        <img width="50" height="50" src={logo} className="img-fluid" alt="Responsive image" />
+                                        <h5 className="pt-1">Hear it. See it. Live it.</h5>
+                                    </div>
+                                </div>
+                                <div className="col-lg-3 col-md-6 footer-links">
+                                    <h5>Useful Links</h5>
+                                    <ul>
+                                        <li>About us</li>
+                                        <li>Join us</li>
+                                        <li>Contact US</li>
+                                    </ul>
+                                </div>
+                                <div className="col-lg-3 col-md-6 footer-contact">
+                                    <h5>Follow Us</h5>
+                                    <a href=""><i class="fab fa-facebook-square fa-3x" ></i></a>
+                                </div>
                             </div>
                         </div>
                     </div>
-
                 </footer>
             </div>
         );
--- client/src/components/layouts/HeaderPage.js
+++ client/src/components/layouts/HeaderPage.js
@@ -7,7 +7,7 @@ class HeaderPage extends Component {
             <div>
                 <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
                     <a className="navbar-brand text-light">
-                        <img src={logo} width="30" height="30" class="d-inline-block align-top" alt="picktune-logo" /> PickTune
+                        <img src={logo} width="30" height="30" className="d-inline-block align-top" alt="picktune-logo" /> PickTune
                     </a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon" />
